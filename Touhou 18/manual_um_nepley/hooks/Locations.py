0ad023d295cbd220dcaae48cae9511590271d695
De Sousa Luciano <58588246+Nepley@users.noreply.github.com>
2024-07-24 19:22:18 +0200

    [Th18] Add pre-hint to shop item

--- Touhou 18/manual_um_nepley/hooks/Locations.py
+++ Touhou 18/manual_um_nepley/hooks/Locations.py
@@ -3,12 +3,4 @@ import random, re
 # called after the locations.json has been imported, but before ids, etc. have been assigned
 # if you need access to the locations after processing to add ids, etc., you should use the hooks in World.py
 def before_location_table_processed(location_table: list) -> list:
-    for location in location_table:
-        # print(location)
-        if 'region' in location:
-            match location["region"]:
-                case "Shop-Item":
-                    price = "("+str(random.randint(1, 40))+"0)"
-                    location["name"] = re.sub("\(.*\)", price, location["name"])
-
     return location_table
